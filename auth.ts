68b183c45bdec3c2a48cf2636fec964058ab1952
AntonioEpichin <tadeuepichin@gmail.com>
2024-05-29 19:02:27 +0000

    configuracao inical do next-auth pronta

--- auth.ts
+++ auth.ts
@@ -1,9 +1,13 @@
-import NextAuth from 'next-auth';
+import NextAuth from "next-auth"
 import Credentials from 'next-auth/providers/credentials';
 
-export const {
-  handlers: { GET, POST },
-  auth,
-} = NextAuth({
-  providers: [Credentials],
-});
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    providers: [
+        Credentials({
+            async authorize() {
+                return { id: '1', name: 'Fulano de Tal', email: 'lala@lala.com' };
+            },
+        }),
+    ],
+})
