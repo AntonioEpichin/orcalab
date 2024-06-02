fbd6b9bddf5e24e8726f65a65b4251cff5f28969
AntonioEpichin <tadeuepichin@gmail.com>
2024-06-02 01:35:39 +0000

    litle fix in         <SpeedInsights />

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ function InnerRootLayout({ children }) {
         <LogoSection />
         <main>
           {children}
+          <SpeedInsights />
         </main>
         <Footer />
       </body>
